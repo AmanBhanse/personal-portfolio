1eea69beb92a0094bf6749d930c529a8e529966c
AmanBhanse <amanbhanse22@gmail.com>
2022-05-10 21:39:53 +0530

    Add file folder structure for frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
 
+import {About, Footer , Header , Skills, Testimonials , Work} from './container';
+import {NavBar} from './components';
 function App() {
   return (
     <div className="App">
-      
+      <NavBar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonials />
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './NavBar.scss'
+const NavBar = () => {
+  return (
+    <div>NavBar</div>
+  )
+}
+
+export default NavBar
