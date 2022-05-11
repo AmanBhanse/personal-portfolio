88d6d412cb5c9431669d4b7c108efdca5079b7db
AmanBhanse <amanbhanse22@gmail.com>
2022-05-11 22:55:48 +0530

    add starter code for navbar component

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 import './NavBar.scss'
+import {images} from '../../constants'
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <nav>
+      <div>
+        <img src={images.logo} alt="Logo"></img>
+      </div>
+      <ul>
+        {['home', 'about' , 'work' , 'skills' , 'contacts'].map( (item)=>{
+          return(
+            <li key={`link-${item}`}>
+              <div/>
+              <a href={`#${item}`}>{item}</a>
+            </li>
+          )
+        } )}
+      </ul>
+    </nav>
   )
 }
 
