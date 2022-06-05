b4452c0bd86c5df85d953dbb9b8cc00bee0cc5ba
AmanBhanse <amanbhanse22@gmail.com>
2022-06-05 11:45:41 +0530

    Implement Footer

--- frontend/src/container/Footer/Footer.jsx
+++ frontend/src/container/Footer/Footer.jsx
@@ -1,10 +1,106 @@
-import React from 'react'
+/* eslint-disable new-cap */
+import React, { useState } from 'react';
+
+import { images } from '../../constants';
+import { MotionWrap, AppWrap } from '../../wrapper';
+import { client } from '../../client';
 import './Footer.scss';
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
 
-export default Footer
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const { name, email, message } = formData;
+
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = () => {
+    setLoading(true);
+
+    const contact = {
+      _type: 'contact',
+      name: name,
+      email: email,
+      message: message,
+    };
+
+    client.create(contact).then(() => {
+      setLoading(false);
+      setIsFormSubmitted(true);
+    });
+  };
+  return (
+    <>
+      <h2 className='head-text'>Take a coffee & chat with me</h2>
+      <div className='app__footer-cards'>
+        <div className='app__footer-card'>
+          <img src={images.email} alt='email' />
+          <a href='mailto:amanbhanse22@gmail.com' className='p-text'>
+            amanbhanse22@gmail.com
+          </a>
+        </div>
+        <div className='app__footer-card'>
+          <img src={images.mobile} alt='mobile' />
+          <a href='tel: +91 (8600966305)' className='p-text'>
+            +91 (8600966305)
+          </a>
+        </div>
+      </div>
+
+      {!isFormSubmitted ? (
+        <div className='app__footer-form app__flex'>
+          <div className='app__flex'>
+            <input
+              className='p-text'
+              type='text'
+              placeholder='Your Name'
+              value={name}
+              name='name'
+              onChange={handleChangeInput}></input>
+          </div>
+
+          <div className='app__flex'>
+            <input
+              className='p-text'
+              type='email'
+              placeholder='Your Email'
+              value={email}
+              name='email'
+              onChange={handleChangeInput}></input>
+          </div>
+
+          <div>
+            <textarea
+              className='p-text'
+              placeholder='Your message'
+              value={message}
+              name='message'
+              onChange={handleChangeInput}></textarea>
+          </div>
+          <button type='button' className='p-text' onClick={handleSubmit}>
+            {loading ? 'Sending' : 'Send Message'}
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h3 className='head-text'> Thank you for getting in touch</h3>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default AppWrap(
+  MotionWrap(Footer, 'app__footer'),
+  'contact',
+  'app__whitebg'
+);
--- frontend/src/container/Testimonials/Testimonials.jsx
+++ frontend/src/container/Testimonials/Testimonials.jsx
@@ -25,7 +25,6 @@ const Testimonials = () => {
 
     client.fetch(brandsQuery).then((data) => {
       setBrands(data);
-      console.log(brands);
     });
   }, []);
 
