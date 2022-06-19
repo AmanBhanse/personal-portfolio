8eb0ff120c8fba65f8f9b016e4a96db4f4f99caf
AmanBhanse <amanbhanse22@gmail.com>
2022-06-19 19:21:59 +0530

    Github social added

--- frontend/src/components/SocialMedia/SocialMedia.jsx
+++ frontend/src/components/SocialMedia/SocialMedia.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { BsTwitter, BsLinkedin } from 'react-icons/bs';
+import { FaGithub } from 'react-icons/fa';
 
 const SocialMedia = () => {
   return (
     <div className='app__social'>
+      <a
+        href='https://github.com/AmanBhanse'
+        target='_blank'
+        rel='noopener noreferrer'>
+        <div>
+          <FaGithub />
+        </div>
+      </a>
       <a
         href='https://www.linkedin.com/in/aman-bhanse/'
         target='_blank'
