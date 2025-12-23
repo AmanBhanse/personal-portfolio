755ab264c7c281c750650a06df65d8cae29d658a
AmanBhanse <amanbhanse22@gmail.com>
2025-12-23 12:31:25 +0100

    format

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -1,10 +1,10 @@
-import sanityClient from '@sanity/client';
-import imageUrlBuilder from '@sanity/image-url';
+import sanityClient from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
 
 export const client = sanityClient({
-  projectId: process.env.REACT_APP_SANITY_PROJECT_ID || 'o5jbqmpw',
-  dataset: 'production',
-  apiVersion: '2022-02-01',
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID || "o5jbqmpw",
+  dataset: "production",
+  apiVersion: "2022-02-01",
   useCdn: true,
   token: process.env.REACT_APP_SANITY_TOKEN,
 });
