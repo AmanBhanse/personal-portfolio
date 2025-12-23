12388932e2b6580682f0c86912759d610f3fef1b
AmanBhanse <amanbhanse22@gmail.com>
2025-12-23 12:31:14 +0100

    Fix Frontend Vulnerabilities

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -1,17 +1,13 @@
 import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
-const sanityCred = require('./sanityCred.json');
-
 export const client = sanityClient({
-  projectId: sanityCred.projectId,
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID || 'o5jbqmpw',
   dataset: 'production',
   apiVersion: '2022-02-01',
   useCdn: true,
-  token: sanityCred.token,
-
+  token: process.env.REACT_APP_SANITY_TOKEN,
 });
-
 
 const builder = imageUrlBuilder(client);
 export const urlFor = (source) => builder.image(source);
